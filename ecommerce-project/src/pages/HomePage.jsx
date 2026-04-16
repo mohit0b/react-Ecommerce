9db54787715295c03bfaa329c97209e6e8c448c9
fundametrics <bharambem450@gmail.com>
2026-04-16 10:36:08 +0530

    Get cart from the backend and display quantity

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,24 +1,30 @@
 import axios from 'axios';
-import {useEffect,useState} from 'react';
+import { useEffect, useState } from 'react';
 import { Header } from '../components/Header'
 import './HomePage.css'
 import CheckmarkIcon from '../assets/images/icons/checkmark.png';
 
 export function HomePage() {
-    const [products,setProducts] = useState([]);
+    const [products, setProducts] = useState([]);
+    const [cart,setCart] = useState([])
     useEffect(() => {
-      axios.get('http://localhost:3000/api/products')
-        .then((response) => {
-            setProducts(response.data)
-        });
-    },[]);
-    
-    
+        axios.get('http://localhost:3000/api/products')
+            .then((response) => {
+                setProducts(response.data)
+            });
+
+        axios.get('http://localhost:3000/api/cart-items')
+            .then((response) => {
+                setCart(response.data)
+            })
+    }, []);
+
+
     return (
         <>
             <title>Ecommerce-project</title>
             <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
-            <Header />
+            <Header cart= {cart}/>
             <div className="home-page">
                 <div className="products-grid">
                     {products.map((product) => {
