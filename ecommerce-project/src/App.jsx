024a358b0a5f6b2812202400b71b58feb00175ec
fundametrics <bharambem450@gmail.com>
2026-04-16 12:41:57 +0530

    generate the HTML for the deliveryOptions

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -15,12 +15,13 @@ function App() {
     axios.get('/api/cart-items?expand=product')
       .then((response) => {
         setCart(response.data)
+        console.log(response.data)
       })
   },[])
   return (
     <Routes>
       <Route index element={<HomePage cart ={cart}/>} />
-      <Route path="checkout" element={<CheckoutPage cart= {cart}/>} />
+      <Route path="checkout" element={<CheckoutPage cart ={cart}/>} />
       <Route path="orders" element={<OrdersPage />} />
       <Route path="tracking" element={<TrackingPage />} />
       <Route path="*" element={<NotFoundPage />}></Route>
