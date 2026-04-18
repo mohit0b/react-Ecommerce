e3238128b6bb15264d5d88d2da3b3d81f57252b5
fundametrics <bharambem450@gmail.com>
2026-04-18 11:44:46 +0530

    convert ecommerce project into react

---
+++ ecommerce-project-ts/src/pages/checkout/CheckoutPage.jsx
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { CheckoutHeader } from './CheckoutHeader';
+import './CheckoutPage.css'
+import { OrderSummary } from './OrderSummary.jsx';
+import { PaymentSummary } from './PaymentSummary.jsx';
+
+
+export function CheckoutPage({ cart,loadCart }) {
+    const [deliveryOptions, setDeliveryOptions] = useState([]);
+    const [paymentSummary, setPaymentSummary] = useState(null)
+
+    useEffect(() => {
+        const fetchCheckoutData = async () =>{
+            let response = await axios.get('/api/delivery-options?expand=estimatedDeliveryTime')
+            setDeliveryOptions(response.data)
+
+
+        response = await axios.get('api/payment-summary')
+            setPaymentSummary(response.data);
+        };
+        
+        fetchCheckoutData();
+    }, [cart])
+    return (
+        <>
+            <title>checkout</title>
+            <link rel="icon" type="image/svg+xml" href="cart-favicon.png" />
+
+            <CheckoutHeader />
+            <div className="checkout-page">
+                <div className="page-title">Review your order</div>
+
+                <div className="checkout-grid">
+                    < OrderSummary cart={cart} deliveryOptions={deliveryOptions}
+                    loadCart={loadCart} />
+
+                    <PaymentSummary paymentSummary={paymentSummary} loadCart= {loadCart}/>
+                </div>
+            </div>
+        </>
+    );
+}
